9d7102ca6d3e537f1d9d8ceac646fbcaca591d7b
Jane <janechodance@gmail.com>
2021-08-24 23:39:09 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const main = document.querySelector('main')
+main.remove();
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.textContent = 'Jane is the champion'
